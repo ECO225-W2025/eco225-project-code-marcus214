80f027b5a31212c3f10e4b74b4793e3704d32d39
Marcus Wang <marcusnh.wang@mail.utoronto.ca>
2025-01-23 23:03:42 +0800

    Add Test file
    Test file

---
+++ test1.py
@@ -0,0 +1,19 @@
+import random
+import numpy as np
+import pandas as pd
+import qeds
+import matplotlib.pyplot as plt
+
+#%matplotlib inline
+# activate plot theme
+import qeds
+
+# Dataframe 
+bagel = pd.DataFrame({
+    "City" : ["Montreal","Montreal","Montreal", "Montreal", "Montreal","Montreal","New York", "New York", "New York", "New York", "New York", "New York"],
+    "BagelProduced" : [5, 10, 20, 25, 30, 35, 3, 13, 23, 23, 33, 33],
+    "Year": [2016, 2017, 2018, 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024, 2025]
+})
+
+# Look at dataframe
+bagel
